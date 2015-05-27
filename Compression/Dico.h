62b1a7009f63d8ff0a3eafdc6db7c3d3fbba69e2
banana38 <anna.bruel@orange.fr>
2015-05-27 23:52:00 +0200

    affichage dans dico et recherche d'une sequence dans un dico

--- Compression/Dico.h
+++ Compression/Dico.h
@@ -7,7 +7,6 @@ typedef uint64_t valeur_t; //valeur attribuée (binaire)
 
 typedef short int caractere_t; //valeur caractère (ASCII)
 
-
 typedef struct _noeud noeud;
 struct _noeud {
 	valeur_t valeur;
@@ -22,6 +21,10 @@ void init_dico(dictionnaire dico);
 
 void ajout_dico(char * sequence, valeur_t valeur, dictionnaire dico, int code_sequence);
 
-valeur_t recherche_dico(char * sequence, dictionnaire dico);
+int recherche_dico(char * sequence, dictionnaire dico);
+
+void parse(char **seq);
+
+char hexa_to_char(uint64_t n);
 
 void afficher(dictionnaire dico);
