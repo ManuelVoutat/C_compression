10131dd3de4168f2acf9e8aa159b3c9d85fff868
Lechevallier <maxime.lechevallier@laposte.net>
2015-05-27 16:53:11 +0200

    Nouveau Dico.h

--- Compression/Dico.h
+++ Compression/Dico.h
@@ -4,13 +4,19 @@ typedef uint64_t valeur_t; //valeur attribuée (binaire)
 
 typedef short int caractere_t; //valeur caractère (ASCII)
 
+typedef noeud * dico;
+
+
 typedef struct _noeud noeud;
 struct _noeud {
 	valeur_t valeur;
 	caractere_t caractere;
 	struct _noeud *frere;
 	struct _noeud *fils;
-}
+};
 
-void ajout_dico(string sequence, valeur_t valeur, noeud * racine);
-valeur_t recherche_dico(string sequence, noeud * racine);
+void init_dico(dico racine);
+
+void ajout_dico(char * sequence, valeur_t valeur, dico racine);
+
+valeur_t recherche_dico(char * sequence, dico racine);
